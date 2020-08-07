56df0a0d75d9ade9581c60d31fbcaf91d5919da2
maksim-luzin <maksim-luzin@users.noreply.github.com>
2020-08-07 17:39:34 +0300

    Create of the initial project structure for the client side.

---
+++ client/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import './styles/reset.scss';
+import './styles/bootstrap.min.css';
+import './styles/page.scss';
+
+const target = document.getElementById('root');
